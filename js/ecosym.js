7242a4811f87f391d2d7fab2fe840183fb98900c
John Eargle <jeargle@gmail.com>
2019-09-14 15:26:45 -0500

    #1 added ContinuousExponential class

--- js/ecosym.js
+++ js/ecosym.js
@@ -1,4 +1,24 @@
 
+
+class ContinuousExponential {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return this.N0*Math.exp(this.r*t)
+    }
+
+    doublingTime() {
+        return Math.log(2)/this.r
+    }
+}
+
+
 $(document).ready(function() {
     'use strict'
 })
