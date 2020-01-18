6d17c3ea94c4cbde7e336cbee746a961d7fc50f6
John Eargle <jeargle@gmail.com>
2020-01-18 17:54:13 -0600

    added stubs for a bunch of models

--- js/ecosym.js
+++ js/ecosym.js
@@ -3,17 +3,18 @@
 const range = (start, stop, step = 1) =>
       Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
 
-class ContinuousExponential {
+
+class Continuous {
     b = 0.11
     d = 0.1
     N0 = 100
+    r = Math.log(2)
 
     constructor() {
-        this.r = this.b - this.d
     }
 
     population(t) {
-        return this.N0*Math.exp(this.r*t)
+        return this.N0
     }
 
     applyToTimespan(timespan) {
@@ -24,6 +25,115 @@ class ContinuousExponential {
         return Math.log(2)/this.r
     }
 }
+
+
+class Discrete {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return
+    }
+
+    applyToTimespan(timespan) {
+        return timespan.map((t) => this.population(t))
+    }
+
+    doublingTime() {
+        return Math.log(2)/this.r
+    }
+}
+
+
+class ContinuousExponential extends Continuous {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        super()
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return this.N0*Math.exp(this.r*t)
+    }
+}
+
+
+class DiscreteExponential {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return
+    }
+
+    applyToTimespan(timespan) {
+        return timespan.map((t) => this.population(t))
+    }
+
+    doublingTime() {
+        return Math.log(2)/this.r
+    }
+}
+
+class EnvironmentalStochasticity {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return
+    }
+
+    applyToTimespan(timespan) {
+        return timespan.map((t) => this.population(t))
+    }
+
+    doublingTime() {
+        return Math.log(2)/this.r
+    }
+}
+
+
+class DemographicStochasticity {
+    b = 0.11
+    d = 0.1
+    N0 = 100
+
+    constructor() {
+        this.r = this.b - this.d
+    }
+
+    population(t) {
+        return
+    }
+
+    applyToTimespan(timespan) {
+        return timespan.map((t) => this.population(t))
+    }
+
+    doublingTime() {
+        return Math.log(2)/this.r
+    }
+}
+
+
 
 
 $(document).ready(function() {
