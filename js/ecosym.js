69e03874f6728c95f51ee0134504174471a635b7
John Eargle <jeargle@gmail.com>
2020-01-18 18:24:03 -0600

    added some code comments

--- js/ecosym.js
+++ js/ecosym.js
@@ -4,15 +4,23 @@ const range = (start, stop, step = 1) =>
       Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
 
 
+/**
+ * Abstract continuous model.
+ */
 class Continuous {
-    b = 0.11
-    d = 0.1
-    N0 = 100
-    r = Math.log(2)
+    b = 0.11   // birth rate
+    d = 0.1    // death rate
+    N0 = 100   // population size at time 0
+    r = Math.log(2)   // instantaneous rate of increase
 
     constructor() {
     }
 
+    /**
+     * population at time t
+     * @param t {number} - time
+     * @return {number}
+     */
     population(t) {
         return this.N0
     }
@@ -27,10 +35,15 @@ class Continuous {
 }
 
 
+/**
+ * Abstract discrete model.
+ */
 class Discrete {
     b = 0.11
     d = 0.1
     N0 = 100
+    rd = null   // discrete growth factor
+    lambda = null   // finite rate of increase
 
     constructor() {
         this.r = this.b - this.d
@@ -50,10 +63,19 @@ class Discrete {
 }
 
 
+/**
+ * Continuous exponential model
+ * assumptions:
+ *   constant birth and death rates
+ *   no genetic structure
+ *   no age or size structure
+ *   continuous growth with no time lags
+ */
 class ContinuousExponential extends Continuous {
-    b = 0.11
-    d = 0.1
-    N0 = 100
+    // b = 0.11   // birth rate
+    // d = 0.1    // death rate
+    // N0 = 100   // population size at time 0
+    // r = null   // instantaneous rate of increase
 
     constructor() {
         super()
@@ -66,12 +88,23 @@ class ContinuousExponential extends Continuous {
 }
 
 
+/**
+ * Discrete exponential model
+ * assumptions:
+ *   constant birth and death rates
+ *   non-overlapping generations
+ *   no age or size structure
+ *   discrete growth with no time lags
+ */
 class DiscreteExponential {
-    b = 0.11
-    d = 0.1
-    N0 = 100
+    // b = 0.11
+    // d = 0.1
+    // N0 = 100
+    // rd = null
+    // lambda = null
 
     constructor() {
+        super()
         this.r = this.b - this.d
     }
 
