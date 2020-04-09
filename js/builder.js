0d4069090b083f3cc831ce1651dd7ff0c43d4829
John Eargle <jeargle@gmail.com>
2020-04-08 19:34:07 -0500

    #6 added Plotter class

--- js/builder.js
+++ js/builder.js
@@ -2,18 +2,60 @@
 /*global console: false, ecosym: false, extend: false*/
 
 
+/**
+ * Abstract continuous-time model.
+ */
+class Plotter {
+    plotId = 'plot'
+    ecoModel = null   // ecosym model
+    timespan = []     // set of timepoints (x-axis)
+
+    constructor(plotId='plot', ecoModel=null, timespan=[]) {
+        this.plotId = plotId
+        this.ecoModel = ecoModel
+        this.timespan = timespan
+    }
+
+    /**
+     * Set ecosym model
+     * @param ecoModel {Object} - ecosym model
+     */
+    setEcoModel(ecoModel) {
+        this.ecoModel = ecoModel
+    }
+
+    /**
+     * Set timespan
+     * @param timespan {array}
+     */
+    setTimespan(timespan) {
+        this.timespan = timespan
+    }
+
+    /**
+     * Plot the ecosym model
+     */
+    plot() {
+        let model = this
+        let plotDiv = document.getElementById(model.plotId)
+        let population = model.ecoModel.applyToTimespan(model.timespan)
+        Plotly.newPlot(
+            plotDiv,
+            [{x: model.timespan, y: population}],
+            {margin: { t: 0 } }
+        )
+    }
+
+}
+
+
 $(document).ready(function() {
     'use strict'
 
-    let ce1 = new ContinuousExponential()
-    let times1 = range(0, 201, 20)
-    let populations1 = ce1.applyToTimespan(times1)
-
-    let plot = document.getElementById('plot')
-    Plotly.newPlot(
-        plot,
-        [{x: times1,
-	  y: populations1}],
-        {margin: { t: 0 } }
+    let plotter1 = new Plotter(
+        'plot',
+        new ContinuousExponential(),
+        range(0, 201, 20)
     )
+    plotter1.plot()
 })
